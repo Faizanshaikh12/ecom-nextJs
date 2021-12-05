ae3f375c1f557ad79c5edf77326153fda0d1466c
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-12-06 01:23:22 +0530

    Make Responsive Header

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Hulu 2.O</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            {/*Header*/}
+            <Header/>
+            {/*Navbar*/}
+            {/*Main*/}
+        </div>
+    )
+}
